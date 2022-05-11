f7626db5a5c52685542bb970736f3f767395eb4c
Andre Rico <97684721+AndreRicoPSU@users.noreply.github.com>
2022-05-11 10:02:42 -0400

    sincr wed 11

--- LAB/chunk/V2 - H5/main.py
+++ LAB/chunk/V2 - H5/main.py
@@ -2,29 +2,25 @@
 from transf_plink_h5 import *
 from transf_h5_pg import *
 import clarite
+import argparse
 
-
-# Enter the steps will be performed:
-usr_extract_plink = False  # Read the plink files and create the .h5 file
-# Read the .h5, convert in panda-genomics and run CUSTOM SCRIPT
-usr_extract_hdf5 = True
 
 # Enter with the user parameters:
 usr_qtd_variants = 1000  # Qtd of variants in each dataset to save in .h5
-usr_cicle_run = 40  # If None will extract all dataset in .h5 to panda-genomics
 usr_category = True
 usr_swap_alleles = False
 
 # Enter with the file name and location:
 usr_file_name = "plink_test_medium"
-int_input = Path(__file__).parent.parent / "plink" / \
-    usr_file_name  # Folder Path to plink
-int_output = Path(__file__).parent.parent / "output" / \
-    usr_file_name  # Folder Path to .h5
+int_input = "/Users/andrerico/DEV/LAB/chunk/plink/" + usr_file_name
+int_output = "/Users/andrerico/DEV/LAB/chunk/output/" + usr_file_name
+#int_input = "/gpfs/group/mah546/default/datasets/simulated_genotypes/" + usr_file_name
+#int_output = "/gpfs/group/mah546/default/datasets/simulated_genotypes/" + usr_file_name
 
 
 def cust_script(df_genomics, v_cicle):
-    output_script = Path(__file__).parent.parent / "output" / "file"
+    #output_script = "/Users/andrerico/DEV/LAB/chunk/output/file"
+    output_script = "/gpfs/group/mah546/default/projects/edge/HDF5_convert/output/result"
     result_clarite = clarite.analyze.association_study(
         data=df_genomics, outcomes="phenotype")
     result_clarite.to_csv(f"{output_script}_clarite_{v_cicle}.txt", sep=";")
@@ -34,16 +30,41 @@ def cust_script(df_genomics, v_cicle):
 
 if __name__ == "__main__":
 
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-s", "--start", type=int,
+                        metavar="dataset",
+                        action="store",
+                        default=None,
+                        help="Inform with dataset will start",
+                        )
+    parser.add_argument("-e", "--end", type=int,
+                        metavar="dataset",
+                        action="store",
+                        default=None,
+                        help="Inform with dataset will end",
+                        )
+
+    parser.add_argument("-p", "--extract_plink", action="store_true", help="")
+    parser.add_argument("-r", "--extract_hdf5", action="store_true", help="")
+
+    args = parser.parse_args()
+
+    #args.extract_plink = True
+    #args.extract_hdf5 = True
+    #args.start = 10
+    #args.end = 19
+
     # Convert plink 1.9 to .h5
-    if usr_extract_plink:
+    # if usr_extract_plink:
+    if args.extract_plink:
         print("> star process of converting plink to .h5")
         result = from_plink(int_input, int_output, usr_qtd_variants)
         print("> end process of conversion plink to .h5 in", result, "datasets")
 
     # Convert .h5 to pandas-genomics
-    if usr_extract_hdf5:
-
-        if usr_cicle_run == None:
+    # if usr_extract_hdf5:
+    if args.extract_hdf5:
+        if args.start == None and args.end == None:
             v_db_cicle = get_cicles(int_output)
             print(">  start process to converting .h5 to pandas-genomics with:",
                   v_db_cicle, "datasets")
@@ -59,17 +80,24 @@ if __name__ == "__main__":
                 # CUSTOM SCRIPT
                 cust_script(df_genomics, v_cicle)
 
+        elif args.start != None and args.end != None:
+            print(">  start process to converting .h5 to pandas-genomics start:",
+                  args.start, "dataset and end:", args.end, "dataset")
+
+            args.end += 1
+            for v_cicle in range(args.start, args.end):
+
+                print(">  start extract dataset",
+                      v_cicle, "to pandas-genomics")
+                df_genomics = get_db(int_output, v_cicle,
+                                     usr_category, usr_swap_alleles)
+                print(">  end extract dataset",
+                      v_cicle, "to pandas-genomics")
+
+                # CUSTOM SCRIPT
+                cust_script(df_genomics, v_cicle)
+
         else:
-            v_db_cicle = usr_cicle_run
-            print(">  start extract dataset",
-                  v_db_cicle, "to pandas-genomics")
-            df_genomics = get_db(int_output, v_db_cicle,
-                                 usr_category, usr_swap_alleles)
-            print(">  end extract dataset",
-                  v_db_cicle, "to pandas-genomics")
-
-            # CUSTOM SCRIPT
-            cust_script(df_genomics, v_db_cicle)
-
+            print("> Informa start and end dataset to process")
 
 print(">END OF PROCESS")
